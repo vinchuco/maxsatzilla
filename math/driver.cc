a40ae5296fc7f3f3f2705146f546962b84d6ba42
Paulo J. Matos <paulo@matos-sorge.com>
2007-11-20 13:14:38 +0000

    Driver now calls FS routine.

--- math/driver.cc
+++ math/driver.cc
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 
 #include "dataset.hh"
+#include "forwardselection.hh"
+
+using std::vector;
+
+#define FIN 0.15
 
 static char* labels[5] = {"Solver1", "Solver2", "Vars", "Clauses", "SomethingElse"};
 
@@ -16,7 +22,14 @@ int main(void) {
   data[2][0] = 90000.0; data[2][1] = 100.0; data[2][2] = 200.0; data[2][3] = 10000.0; data[2][4] = 950.2;
 
   MSZDataSet *ds = createDataSet(data, 3, 5, 2);
+
+  // Lets create the plot files
   ds->dumpPlotFiles(labels, 5, "./driver");
+
+  // Lets do a forward selection
+  ForwardSelection fs(*ds, 0);
+
+  vector<int> res = fs.run(FIN);
 
   return 0;
 }
