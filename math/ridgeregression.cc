5905840820aa84593c81bb5d642b70b522a17379
Paulo J. Matos <paulo@matos-sorge.com>
2007-11-21 19:18:35 +0000

    Initial files for ridge regression implementation.

---
+++ math/ridgeregression.cc
@@ -0,0 +1,2 @@
+#include "ridgeregression.hh"
+
---
+++ math/ridgeregression.hh
@@ -0,0 +1,17 @@
+#ifndef RIDGEREGRESSION_HH
+#define RIDGEREGRESSION_HH
+
+/** This class implements
+ *  the ridge regression algorithm.
+ */
+class RidgeRegression {
+public:
+  RidgeRegression();
+
+  
+
+private:
+
+};
+
+#endif // RIDGEREGRESSION_HH
