4475a58bc79632842b39edf1bbe3a02182ab6277
Paulo J. Matos <paulo@matos-sorge.com>
2007-11-23 17:32:13 +0000

    Header file to match the rest .hh

--- math/driver.cc
+++ math/driver.cc
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <vector>
 
+#include "../mszparse.h"
+
 #include "dataset.hh"
 #include "forwardselection.hh"
+#include "ridgeregression.hh"
 
 using std::vector;
 
 #define FIN 0.15
 
-static char* labels[5] = {"Solver1", "Solver2", "Vars", "Clauses", "SomethingElse"};
-
 int main(void) {
 
-  double **data = 0;
-  data = new double*[3];
-  for(int i = 0; i < 3; i++)
-    data[i] = new double[5];
+  
 
-  data[0][0] = 900.0; data[0][1] = 1.0; data[0][2] = 100.0; data[0][3] = 100.0; data[0][4] = 120.3;
-  data[1][0] = 9000.0; data[1][1] = 10.0; data[1][2] = 150.0; data[1][3] = 1000.0; data[1][4] = 99.5;
-  data[2][0] = 90000.0; data[2][1] = 100.0; data[2][2] = 200.0; data[2][3] = 10000.0; data[2][4] = 950.2;
 
   MSZDataSet *ds = createDataSet(data, 3, 5, 2);
 
