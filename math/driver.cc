fb229682c79c4354ce87f8bd3576eafe1ce8abdb
Paulo J. Matos <paulo@matos-sorge.com>
2007-11-23 17:48:15 +0000

    Updated driver to use parser.

--- math/driver.cc
+++ math/driver.cc
@@ -1,25 +1,65 @@
 #include <iostream>
 #include <vector>
 
-#include "../mszparse.h"
+#include <zlib.h>
+
+#include "../mszreader.hh"
 
 #include "dataset.hh"
 #include "forwardselection.hh"
 #include "ridgeregression.hh"
 
 using std::vector;
+using std::string;
+using std::cerr;
+
+using namespace iomsz;
 
 #define FIN 0.15
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
+   if (argc <= 1) exit(EXIT_FAILURE);
+
+  unsigned int nbSolvers, nbFeatures, nbInstances, timeOut;
+  string *solversNames;
+  string *featuresNames;
+  string *instancesNames;
+  double **data;
   
+  char *inputFileName = argv[1];
 
+  gzFile in=(inputFileName==NULL? gzdopen(0,"rb"): gzopen(inputFileName,"rb"));
+  if (in==NULL) {
+    cerr<<"Error: Could not open file: "
+        <<(inputFileName==NULL? "<stdin>": inputFileName)
+        <<endl;
+    exit(1);
+  }
+
+  parse_DIMACS(in, nbSolvers, nbFeatures, nbInstances, timeOut, solversNames, featuresNames, instancesNames, data);
+
+  cerr << "Read file: " << inputFileName << "\n"
+       << "Number of Solvers: " << nbSolvers << "\n"
+       << "Number of Features: " << nbFeatures << "\n"
+       << "Timeout: " << timeOut << "\n";
+
+  cerr << "Solver Names: ";
+  for(size_t i = 0; i < nbSolvers; i++)
+    cerr << solversNames[i] << " ";
+
+  cerr << "\nFeature Names: ";
+  for(size_t i = 0; i < nbFeatures; i++)
+    cerr << featuresNames[i] << " ";
+  
+  cerr << "\nInstance Names: ";
+  for(size_t i = 0; i < nbInstances; i++)
+    cerr << instancesNames[i] << " ";
 
   MSZDataSet *ds = createDataSet(data, 3, 5, 2);
 
   // Lets create the plot files
-  ds->dumpPlotFiles(labels, 5, "./driver");
+  //ds->dumpPlotFiles(labels, 5, "./driver");
 
   // Lets do a forward selection
   ForwardSelection fs(*ds, 0);
