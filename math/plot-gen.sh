c42ff86a4a589f69537e9ac2e8139c12212ad8e7
Paulo J. Matos <paulo@matos-sorge.com>
2007-11-26 15:57:22 +0000

    Now plots all first and second order regressions on the data.

--- math/plot-gen.sh
+++ math/plot-gen.sh
@@ -14,12 +14,16 @@ do
     YAXIS=${STRIPPEDFILE%_*}
     XAXIS=${STRIPPEDFILE#*_}
     
-    rm -f $i.png $i.gp
-    touch $i.png
-    echo "set terminal png medium size 640,480" >> $i.gp
-    echo "set xlabel \"$XAXIS\"" >> $i.gp
-    echo "set ylabel \"$YAXIS\"" >> $i.gp
-    echo "plot \"$i\"" >> $i.gp
-    gnuplot $i.gp >> $i.png
+    rm -f $FILEPATH/$i.png $TMPDIR/$i.gp
+    touch $TMPDIR/$i.gp
+    echo "set terminal png medium size 640,480" >> $TMPDIR/$i.gp
+    echo "set xlabel \"$XAXIS\"" >> $TMPDIR/$i.gp
+    echo "set ylabel \"$YAXIS\"" >> $TMPDIR/$i.gp
+    echo "fit m1 * x + b1 \"$i\" via m1, b1" >> $TMPDIR/$i.gp
+    echo "fit a2 + b2 * x + c2 * (x ** 2) \"$i\" via a2, b2, c2" >> $TMPDIR/$i.gp
+    echo "plot \"$i\", \
+          m1 * x + b1, \
+          a2 + b2 * x + c2 * (x ** 2)" >> $TMPDIR/$i.gp
+    gnuplot $TMPDIR/$i.gp >> $FILEPATH/$i.png
 done
     
