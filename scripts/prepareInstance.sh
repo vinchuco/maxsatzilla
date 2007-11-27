8d5cb8cdea3c85aada8c28d2c052a2db2d2e7d4c
Florian Letombe <f.letombe@googlemail.com>
2007-11-27 10:59:23 +0000

    prepareInstance.sh script takes into account .cnf, .cnf.gz, .dimacs, .dimacs.gz & .dimacs.seq

--- scripts/prepareInstance.sh
+++ scripts/prepareInstance.sh
@@ -8,13 +8,28 @@ instance=$1
 
 # Compute base name of instance file
 instanceDirectory=`dirname $instance`
-if [ `basename $instance .cnf` == `basename $instance` ] ; then
-    instanceName=`basename $instance .cnf.gz`
-    extension="cnf.gz"
-else
+if [ `basename $instance .cnf` != `basename $instance` ] ; then
     instanceName=`basename $instance .cnf`
     extension="cnf"
+elif [ `basename $instance .cnf.gz` != `basename $instance` ] ; then
+    instanceName=`basename $instance .cnf.gz`
+    extension="cnf.gz"
+elif [ `basename $instance .dimacs` != `basename $instance` ] ; then
+    instanceName=`basename $instance .dimacs`
+    extension="dimacs"
+elif [ `basename $instance .dimacs.gz` != `basename $instance` ] ; then
+    instanceName=`basename $instance .dimacs.gz`
+    extension="dimacs.gz"
+elif [ `basename $instance .dimacs.seq` != `basename $instance` ] ; then
+    instanceName=`basename $instance .dimacs.seq`
+    extension="dimacs.seq"
+else
+    $instanceName="Unrecognised Format"
 fi
 adimacsInstance="$instanceDirectory/$instanceName.$extension"
+if [ ! -e $adimacsInstance ] ; then
+    echo "$adimacsInstance not found"
+    exit
+fi
 echo "$instanceName"
 echo "$instanceName" >> $logFile
