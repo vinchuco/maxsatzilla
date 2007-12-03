4f651d82abee551d79955f2796e5ac915e41619f
Jordi Planes <jplanes@diei.udl.cat>
2007-12-03 20:29:01 +0000

    Removing \n from path

--- scripts/getfeaturesset.py
+++ scripts/getfeaturesset.py
@@ -10,6 +10,7 @@ for line in open( instance_list ):
     if line[0] == '#':
         continue
     name, path = line.split(' ')
+    path = path[0:-1] # Removing \n
     print "Obtaining features from " + name
     for file in glob.glob( path ):
         print getfeatures_exec + file + '>' + target_directory + '/' + name + '.' + name + '.features'
