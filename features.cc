49cab42d8cefe868eaf4605681263c3744dca43c
Jordi Planes <jplanes@diei.udl.cat>
2007-12-03 13:14:06 +0000

    User info for features

--- features.cc
+++ features.cc
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main( int argc, char* argv[] ) {
-  if ( argc <= 1 ) exit(-1);
+  if ( argc <= 1 ) {
+    printf("usage: %s instance-file\n", argv[0] );
+    exit(0);
+  }
 
   MaxSatInstance msi( argv[1] );
   msi.computeLocalSearchProperties();
