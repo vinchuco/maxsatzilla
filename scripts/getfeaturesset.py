25921953469e2f4290f433123a24a673605d3bc2
Jordi Planes <jplanes@diei.udl.cat>
2007-12-03 20:15:36 +0000

    Adding script to get features

---
+++ scripts/getfeaturesset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys, glob
+
+instance_list = sys.argv[1]
+target_directory = sys.argv[2]
+getfeatures_exec = './getfeatures '
+
+for line in open( instance_list ):
+    if line[0] == '#':
+        continue
+    name, path = line.split(' ')
+    print "Obtaining features from " + name
+    for file in glob.glob( path ):
+        print getfeatures_exec + file + '>' + target_directory + '/' + name + '.' + name + '.features'
+        #os.system( getfeatures_exec + file + '>' + target_directory + '/' + name + '.' + name + '.features' )
+            
