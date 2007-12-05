4bc169d2b6d85f76efce183d5acc821229056b18
Jordi Planes <jplanes@diei.udl.cat>
2007-12-05 15:31:20 +0000

    Improving the script

--- scripts/getfeaturesset.py
+++ scripts/getfeaturesset.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import sys, glob, os
+import sys
+
+if len( sys.argv ) < 3:
+    print "usage " + sys.argv[0] + " <instance list file> <directory>"
+    exit(0)
+
+import glob, os
 
 instance_list = sys.argv[1]
 target_directory = sys.argv[2]
@@ -9,8 +15,7 @@ getfeatures_exec = './getfeatures '
 for line in open( instance_list ):
     if line[0] == '#':
         continue
-    name, path = line.split(' ')
-    path = path[0:-1] # Removing \n
+    name, path, number = line.split(' ')
     print "Obtaining features from " + name
     for file in glob.glob( path ):
         os.system( getfeatures_exec + file + ' > ' + target_directory + '/' + name + '.' + os.path.basename(file) + '.features' )
