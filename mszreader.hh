79817c851a8e1e222756fe55f72be017ebe8dd17
Jordi Planes <jplanes@diei.udl.cat>
2007-12-10 10:58:06 +0000

    Remove z library

--- mszreader.hh
+++ mszreader.hh
@@ -1,32 +1,45 @@
 /** 
- * @file mszreader.h Defines functions for reading a msz file
+ * @file mszreader.hh Defines functions for parsing an msz file
  *
- * @author Florian Letombe
+ * @author Florian Letombe & Paulo Matos & Jordi Planes
  * @since November, 2007
+ *
+ * Pieces of code extracted from MiniSat 
+ *
+ * MiniSat -- Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+ * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef MSZREADER__H__
 #define MSZREADER__H__
 
-#include <ctime>
-#include <cstring>
-#include <cassert>
-#include <stdint.h>
-#include <errno.h>
+//#include <ctime>
+//#include <cstring>
+//#include <cassert>
+//#include <stdint.h>
+//#include <errno.h>
 
-#include <signal.h>
-#include <zlib.h>
+//#include <signal.h>
+//#include <zlib.h>
 
-#include <string>
+//#include <string>
 
 using namespace std;
 
-/** 
- * Defines functions for input/output of DIMACS files.
- *
- * @author Florian Letombe
- * @since November, 2007
-*/
 namespace iomsz {
 
   /*************************************************************************************/
@@ -66,32 +79,45 @@ namespace iomsz {
 
 #define CHUNK_LIMIT 1048576
 #define reportf(format, args...) ( fflush(stdout), fprintf(stderr, format, ## args), fflush(stderr) )
-
+  /*
   class StreamBuffer {
-    gzFile  in;
+    istream&  in;
     char    buf[CHUNK_LIMIT];
     int     pos;
     int     size;
 
+  public:
+  StreamBuffer(istream& i) : in(i), pos(0), size(0) { }
+
+    int  operator *  () { return (pos >= size) ? EOF : buf[pos];}
+    void operator ++ () { pos++; }
+  };
+  */
+  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  class StreamBuffer {
+    istream&  in;
+    char    buf[CHUNK_LIMIT];
+    int     pos;
+    int     size;
+    
     void assureLookahead() {
       if (pos >= size) {
-	pos  = 0;
-	size = gzread(in, buf, sizeof(buf));
+       pos  = 0;
+       size = in.readsome( buf, sizeof(buf));
       }
     }
-
+    
   public:
-  StreamBuffer(gzFile i) : in(i), pos(0), size(0) { assureLookahead(); }
-
+    StreamBuffer(istream& i) : in(i), pos(0), size(0) { assureLookahead(); }
+    
     int  operator *  () { return (pos >= size) ? EOF : buf[pos];}
     void operator ++ () { pos++; assureLookahead(); }
   };
-
-  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+  
   template<class B>
     static void skipWhitespace(B& in) {
-    while ((*in >= 9 && *in <= 13) || *in == 32)
+    while ((*in >= 9 && *in <= 13) || *in == ' ')
       ++in;
   }
 
@@ -133,7 +159,6 @@ namespace iomsz {
     int     valReal, nbDec=0;
     double valDecimal;
     valReal=parseInt(in);
-    //reportf("valReal: %d\n", valReal);
 
     if (*in == '.') {
       ++in;
@@ -149,7 +174,7 @@ namespace iomsz {
   template<class B>
     static void readWord(B& in, string& word) {
     skipWhitespace(in);
-    for (; (*in < 9 || *in > 13) && *in != 32;) {
+    for (; (*in < 9 || *in > 13) && *in != ' ';) {
       word+=*in;
       ++in;
     }
@@ -163,6 +188,52 @@ namespace iomsz {
     return true;
   }
 
+  template<class B>
+     static void parse_MSZ_parameters(B& in,
+				      unsigned int& nbSolvers,
+				      unsigned int& nbFeatures,
+				      unsigned int& nbInstances,
+				      unsigned int& timeOut, 			       
+				      string*& solversNames,
+				      string*& featuresNames,
+				      string*& instancesNames,
+				      double**& matrix) {
+    ++in;++in;
+    if ( match(in, "msz") ) {
+      nbSolvers = parseInt(in);
+      solversNames=new string [nbSolvers];
+      nbFeatures = parseInt(in);
+      featuresNames=new string [nbFeatures];
+      nbInstances = parseInt(in);
+      instancesNames=new string [nbInstances];
+      
+      const int nbCases = nbSolvers+nbFeatures;
+      matrix=new double* [nbInstances];
+      for (unsigned int i=0; i<nbInstances; i++)
+	matrix[i]=new double [nbCases];
+      timeOut = parseInt(in);
+      reportf("c Number of solvers:    %2d\n", nbSolvers);
+      reportf("c Number of features:   %2d\n", nbFeatures);
+      reportf("c Number of instances:  %2d\n", nbInstances);
+      reportf("c Timeout:              %2d\n", timeOut);
+    } else if ( match(in, "slv") ) {
+      reportf("c Solvers:");
+      for (unsigned int i=0; i<nbSolvers; i++) {
+	readWord(in,solversNames[i]);
+	reportf(" %s", solversNames[i].c_str());
+      }
+      reportf("\n");
+    }
+    else if ( match(in, "ftr") ) {
+      reportf("c Features:");
+      for (unsigned int i=0; i<nbFeatures; i++) {
+	readWord(in,featuresNames[i]);
+	reportf(" %s", featuresNames[i].c_str());
+      }
+      reportf("\n");
+    }
+    else reportf("PARSE ERROR! (parse_MSZ) Unexpected params chars: %c %c %c\n", *in, *in, *in), exit(3);
+  }
 
   template<class B>
     static void parse_MSZ_main(B& in,
@@ -174,68 +245,34 @@ namespace iomsz {
 			       string*& featuresNames,
 			       string*& instancesNames,
 			       double**& matrix) {
-    
-    // Simple solving of previous bug
-    match(in, "p msz");
-    nbSolvers = parseInt(in);
-    solversNames=new string [nbSolvers];
-    nbFeatures = parseInt(in);
-    featuresNames=new string [nbFeatures];
-    nbInstances = parseInt(in);
-    instancesNames=new string [nbInstances];
-    
-    const int nbCases = nbSolvers+nbFeatures;
-    matrix=new double* [nbInstances];
-    for (unsigned int i=0; i<nbInstances; i++)
-      matrix[i]=new double [nbCases];
-    timeOut = parseInt(in);
-    reportf("c Number of solvers:    %2d\n", nbSolvers);
-    reportf("c Number of features:   %2d\n", nbFeatures);
-    reportf("c Number of instances:  %2d\n", nbInstances);
-    reportf("c Timeout:              %2d\n", timeOut);
-
-
-    for(unsigned int inst = 0; inst < nbInstances;){
+    unsigned int inst = 0;
+    for(;;){
       skipWhitespace(in);
-      //reportf("caractere lu : %c\n", *in);
-      //(*in == 'q') ? reportf("OK\n") : reportf("KO\n");
-
       if (*in == EOF)
 	break;
       else if (*in == 'p'){
-	++in;++in;
-	//reportf("Current character: %c\n", *in);
-
-	if ((*in=='s') && (match(in, "slv"))) {
-	  reportf("c Solvers:");
-	  for (unsigned int i=0; i<nbSolvers; i++) {
-	    readWord(in,solversNames[i]);
-	    reportf(" %s", solversNames[i].c_str());
-	  }
-	  reportf("\n");
-	}
-	else if ((*in=='f') && (match(in, "ftr"))) {
-	  reportf("c Features:");
-	  for (unsigned int i=0; i<nbFeatures; i++) {
-	    readWord(in,featuresNames[i]);
-	    reportf(" %s", featuresNames[i].c_str());
-	  }
-	  reportf("\n");
-	}
-	else reportf("PARSE ERROR! (parse_MSZ) Unexpected char: %c %c %c\n", *in, *in, *in), exit(3);
+	parse_MSZ_parameters( in, nbSolvers, nbFeatures, nbInstances, timeOut, solversNames, featuresNames, instancesNames, matrix );
       }
-      else {
+      else if (*in == 'c') 
+	skipLine(in); 
+      else if (*in == 'i') {
+	++in;
 	readWord(in,instancesNames[inst]);
-	reportf("Instance: %s\n", instancesNames[inst].c_str());
-
-	reportf("matrix:\n");
+	reportf("c Instance: %s\n", instancesNames[inst].c_str());
+      } else {
+	printf("Current character : %c\n", *in );
+	reportf("c matrix:\n");
 	for (unsigned int i=0; i<nbSolvers; i++) {
 	  matrix[inst][i]=parseDouble(in);
 	  reportf(" %f", matrix[inst][i]);
+	  printf("Iteration %d\n", i);
 	}
+	reportf("\n");
+	skipWhitespace(in);
 	for (unsigned int i=nbSolvers; i<nbSolvers+nbFeatures; i++) {
 	  matrix[inst][i]=parseDouble(in);
 	  reportf(" %f", matrix[inst][i]);
+	  printf("Iteration %d\n", i);
 	}
 	reportf("\n");
 	inst++;
@@ -245,7 +282,7 @@ namespace iomsz {
 
   // Inserts problem into solver.
   //
-  static void parse_DIMACS(gzFile input_stream,
+  static void parse_DIMACS(istream& input_stream,
 			   unsigned int& nbSolvers,
 			   unsigned int& nbFeatures,
 			   unsigned int& nbInstances,
