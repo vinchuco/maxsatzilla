a9f1b832aacb0918a09c9fd1d49b2d480602146d
Jordi Planes <jplanes@diei.udl.cat>
2008-01-09 16:49:21 +0000

    Improvements

--- run-instance-set.py
+++ run-instance-set.py
@@ -1,31 +1,28 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
-import sys, getopt, os
+import sys, os, getopt, glob, os.path
 
-opts, args = getopt.getopt( sys.argv, 'p:' )
+#opts, args = getopt.getopt( sys.argv, 'p:' )
 
-solver = args[0]
-set_list = args[1]
-percentatge = int( opts[0][1] )
+solver = sys.argv[1]
+set_list = sys.argv[2]
+percentatge = 50 #int( opts[0][1] )
+timeout = '1000'
+memout = '2000000'
 
-print solver
-print set_list
-
-exit
-
-sys_limits = 'ulimit -t ' + timeout + '; ulimit -m ' + memout + '; ulimit -d ' + memout + + '; ulimit -v ' + memout + ';'
+sys_limits = 'ulimit -t ' + timeout + '; ulimit -m ' + memout + '; ulimit -d ' + memout + '; ulimit -v ' + memout + ';'
 
 for set in open( set_list ):
     if set[0] == '#':
         continue
     name, path, number = set.split()
-    print 'Running ' + solver + ' with ' + name
     counter = 0
-    times = number * percentatge / 100
-    outfile = solver + '.' + name + '.out'
+    times = int( number ) * percentatge / 100
+    print 'Running ' + solver + ' with ' + name + ' times ' + str( times )
+    outfile = os.path.basename( solver ) + '.' + name + '.out'
     os.system( 'touch ' + outfile )
     for file in glob.glob( path ):
         if counter < times:
             counter += 1
             print solver + ' with ' + file 
-            os.system( solver + ' ' + file + '>> ' + outfile )
+            os.system( sys_limits + 'time -p ' + solver + ' ' + file + '&> ' + outfile +'2>&1')
