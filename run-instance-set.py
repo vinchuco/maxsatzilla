727677980ed95e1c8b1da2c7cab318865b2a03b3
Jordi Planes <jplanes@diei.udl.cat>
2008-01-09 19:20:36 +0000

    Adding execution list

--- run-instance-set.py
+++ run-instance-set.py
@@ -6,6 +6,7 @@ opts, args = getopt.getopt( sys.argv[1:], 'p:' )
 
 solver = args[0]
 set_list = args[1]
+execution_list = args[2:]
 if len( opts ) == 1:
     print '# Percentatge = ' + opts[0][1]
     percentatge = int( opts[0][1] )
@@ -22,16 +23,17 @@ for set in open( set_list ):
     if set[0] == '#':
         continue
     name, path, number = set.split()
-    counter = 0
-    times = int( number ) * percentatge / 100
-    total_instances_run += times
-    print '# Running ' + solver + ' with ' + name + ' ' + str( times ) + '/' + number
-    outfile = os.path.basename( solver ) + '.' + name + '.out'
-    os.system( 'cp /dev/null ' + outfile )
-    for file in glob.glob( path ):
-        if counter < times:
-            counter += 1
-            print '# Executing ' + solver + ' ' + file 
-            os.system( sys_limits + '/usr/bin/time -p ' + solver + ' ' + file + ' >> ' + outfile +' 2>&1')
+    if name in execution_list:
+        counter = 0
+        times = int( number ) * percentatge / 100
+        total_instances_run += times
+        print '# Running ' + solver + ' with ' + name + ' ' + str( times ) + '/' + number
+        outfile = os.path.basename( solver ) + '.' + name + '.out'
+        os.system( 'cp /dev/null ' + outfile )
+        for file in glob.glob( path ):
+            if counter < times:
+                counter += 1
+                print '# Executing ' + solver + ' ' + file 
+                os.system( sys_limits + '/usr/bin/time -p ' + solver + ' ' + file + ' >> ' + outfile +' 2>&1')
 
 print '# Total instances run ' + str( total_instances_run )
