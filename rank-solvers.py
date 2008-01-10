2d3eeca86cad9f3497e1dd3f4e1639c902946b35
Jordi Planes <jplanes@diei.udl.cat>
2008-01-10 16:27:32 +0000

    Improving script

--- rank-solvers.py
+++ rank-solvers.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-import sys
+import sys, getopt
 
-solver_times_path = sys.argv[1]
-set_list = sys.argv[2:]
+opts, args = getopt.getopt( sys.argv[1:], 'v' )
+
+solver_times_path = args[0]
+set_list = args[1:]
+
+verbose = False
+for option in opts:
+    if option[0] == '-v':
+        verbose = True
 
 solvers = [ 'maxsatz-a-e', 'mspbo-a-e', 'msuncore-a1-ei' ]
 times = { 'maxsatz-a-e' : 0.0, 'mspbo-a-e' : 0.0, 'msuncore-a1-ei' : 0.0 }
@@ -18,5 +25,6 @@ for set in set_list:
         for line in open( solver_times_path + '/' + solver + '-' + set + '.log' ):
             instance, solution, time = line.split()
             times[ solver ] += float( time )
-        print 'Total time for solver ' + solver + ' in ' + set + ' ' + str( times[ solver ] )
+        if verbose:
+            print 'Total time for solver ' + solver + ' in ' + set + ' ' + str( times[ solver ] )
     print 'Ranking for ' + set + ' = ' + str( sort_solvers( times ) )
