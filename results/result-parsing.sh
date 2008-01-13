190184f81d24b893b45a3ddae993cd30ed6165d4
Paulo J. Matos <paulo@matos-sorge.com>
2008-01-13 22:13:29 +0000

    Cleaning up maxsatzilla.

--- results/result-parsing.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-for i in `seq 1 34`
-do 
-    for j in `grep "Squared Sum of Errors for the model on the test data" industrial.results.fsinst-$i | cut -c 54- -`
-    do 
-	echo -n $j " " >> ../table
-    done
-    echo >> ../table
-done
