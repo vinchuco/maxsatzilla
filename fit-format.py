3c42a4f76cd5d22e8cb90cadad56cc57596a8ec5
Jordi Planes <jplanes@diei.udl.cat>
2008-01-14 12:23:42 +0000

    Adapting new format

---
+++ fit-format.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys, os, tempfile
+
+if len( sys.argv ) < 2:
+    print "usage : " + os.path.basename( sys.argv[0] ) + " <file>.log"
+    sys.exit(0)
+
+newlogfile = tempfile.NamedTemporaryFile()
+for set in open( sys.argv[1] ):
+    name, time = set.split()
+    print >> newlogfile, name + ' xxx ' + time
+os.rename( newlogfile.name, sys.argv[1] )
+
