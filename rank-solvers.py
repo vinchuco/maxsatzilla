5832a56eb718275a50fb6a18c8bcc74f710dab02
Jordi Planes <jplanes@diei.udl.cat>
2008-01-14 15:38:50 +0000

    Improved

--- rank-solvers.py
+++ rank-solvers.py
@@ -2,21 +2,36 @@
 
 import sys, getopt
 
-opts, args = getopt.getopt( sys.argv[1:], 'v' )
+if len( sys.argv ) == 1:
+    print 'usage ' + sys.argv[0] + ' [options] <set-list>'
+    print """    options: -v : verbose'
+                -d <path to solver times>'
+                -s <solver list file>"""
+    exit( 0 )
 
-solver_times_path = args[0]
-set_list = args[1:]
+opts, args = getopt.getopt( sys.argv[1:], 'vd:s:' )
+
+solver_times_path = './coaching/solver-times/'
+solver_list_file = './coaching/maxsat.solvers'
+set_list = args
 
 verbose = False
 for option in opts:
     if option[0] == '-v':
         verbose = True
+    if option[0] == '-d':
+        solver_times_path = option[1]
+    if option[0] == '-s':
+        solver_list_file = option[1]
 
-times = { 'maxsatz-a-e' : 0.0, 'mspbo-a-e' : 0.0, 'msuncore-a1-ei' : 0.0, 'maxsatzilla' : 0.0 }
+times = {} 
+for solver in open( solver_list_file ):
+    if solver[0] == '#':
+        continue
+    times[ solver[0:-1] ] = 0.0
 
-def sort_solvers( times ):
-    from operator import itemgetter
-    return [ solver for solver,value in sorted(times.items(), key=itemgetter(1)) ]
+def sort_solvers( times ):    
+    return [ solver for solver,value in sorted( times.items(), lambda (x1,x2), (y1,y2) : cmp(x2, y2)) ]
 
 for set in set_list:
     for solver in times.keys():        
