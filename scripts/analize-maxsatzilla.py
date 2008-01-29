aaccfbac635005046d211de79ff191df4fdd9957
Jordi Planes <jplanes@diei.udl.cat>
2008-01-29 17:42:28 +0000

    Improved

--- scripts/analize-maxsatzilla.py
+++ scripts/analize-maxsatzilla.py
@@ -20,11 +20,13 @@ for solver in open( solvers_file ):
         continue
     solvers.append( solver[0:-1] )
     
+total_failures = 0
+total_number = 0
 for line in open( sets_file ):
     if line[0] == '#':
         continue
     set_name, path, number = line.split()
-    instance_path = os.path.dirname( path )
+    instance_path = os.path.dirname( path )    
 
     instance_time = {} 
     for solver in solvers:
@@ -38,14 +40,25 @@ for line in open( sets_file ):
         continue
 
     failures = 0
+    checking_state = 0
     for line in open( 'outputs/maxsatzilla.' + set_name + '.out' ):    
         if line.startswith( instance_path ):
             instance_name = os.path.basename( line[0:-1] )
         #print instance_name
             best_solver = find_best_solver( instance_name )
+            if checking_state != 0:
+                print 'Error : no solver run'
+            checking_state = 1
         if line.startswith( '** Runnning' ):
+            if checking_state != 1:
+                continue
             run_solver = line.split()[2]
             if run_solver not in best_solver[0].split():
                 failures += 1
             #print 'Best solver ' + best_solver[0] + ' but run solver ' + run_solver
+            checking_state = 0
     print 'Failures for ' + set_name + ' = ' + str( failures ) + '/' + number
+    total_failures += failures
+    total_number += int( number )
+
+print 'Total Failures : ' + str( total_failures ) + '/' + str( total_number )
