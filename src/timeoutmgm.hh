f3f5652391200223d0c855aec0e719e2b5dbb476
Paulo J. Matos <paulo@matos-sorge.com>
2008-07-09 10:48:49 +0000

    Timeout manager in header.

---
+++ src/timeoutmgm.hh
@@ -0,0 +1,12 @@
+#ifndef TIMEOUTMGM_HH
+#define TIMEOUTMGM_HH
+
+/*
+ * This class implements the management of timeouts as
+ * censored data as described in the paper
+ * "A simple method for regression analysis with censored data"
+ * - Schmee and Hahn
+ * Technometrics 1979
+ */
+
+#endif // TIMEOUTMGM_HH
