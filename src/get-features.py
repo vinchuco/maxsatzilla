a307443b61e6fe7d4647a49607f714e6e35da5dc
Jordi Planes <jplanes@diei.udl.cat>
2008-08-08 11:51:01 +0000

    Adding path to the executable

--- src/get-features.py
+++ src/get-features.py
@@ -3,11 +3,12 @@
 import sys, os, glob
 
 list_of_benchs = sys.argv[1]
+path = sys.argv[2]
 
 for dir in open( list_of_benchs ):
    if dir.startswith( '#' ):
       continue
    dir = dir.strip()
    for file in glob.glob( dir + '*.cnf' ):
-      print os.popen( './getfeatures ' + file ).readlines()
+      print os.popen( path + './getfeatures ' + file ).readlines()
       
